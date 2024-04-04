15e33026040b220409b76a1e3c7d515e3df4d43f
rochankhristi99 <rochankhristi99@gmail.com>
2024-04-04 09:00:28 +0300

    added

--- src/app/LineFollower.java
+++ src/app/LineFollower.java
@@ -1,5 +1,6 @@
 package app;
 import lejos.hardware.Sound;
+import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
@@ -7,6 +8,7 @@ import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.RegulatedMotor;
 import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
+import lejos.utility.Stopwatch;
 
 
 public class LineFollower extends Thread {
@@ -14,10 +16,13 @@ public class LineFollower extends Thread {
     private static final int MAX_SPEED = 500;
     private static final int BASE_SPEED = 100;
     private static final int SENSOR_THRESHOLD = 25;
+    Stopwatch stopwatch; // Declare stopwatch as a field
+    private int detectionCount = 0;
 
     private EV3ColorSensor lightSensor; 
     private SampleProvider sampleProvider;
     private float[] sample;
+    BackgroundMusicPlayer musicPlayer; // Declare music player as a field
 
     RegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.D);
     RegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.C);
@@ -28,16 +33,18 @@ public class LineFollower extends Thread {
         lightSensor = new EV3ColorSensor(SensorPort.S3);
         sampleProvider = lightSensor.getRedMode();
         sample = new float[sampleProvider.sampleSize()];
-
+        musicPlayer = new BackgroundMusicPlayer(); // Initialize music player
+        stopwatch = new Stopwatch(); 
     }
 
     public void run() {
+    	 musicPlayer.start();
+
         while (true) {
         	
             sampleProvider.fetchSample(sample, 0);
             int sensorValue = (int) (sample[0] * 100); // Convert sensor reading to percentage
-            //System.out.println("Color Value: " + sensorValue);
-            
+      
            /* if(DEObj.getCMD()==0)
             {
                 Sound.twoBeeps();
@@ -51,7 +58,7 @@ public class LineFollower extends Thread {
 				
 				OR
 				
-				  Sound.twoBeeps();
+				Sound.twoBeeps();
                 Sound.twoBeeps();
                 
                 //Turn left side
@@ -64,19 +71,129 @@ public class LineFollower extends Thread {
             */
             if(DEObj.getCMD()==0)
             {
-                Sound.twoBeeps();
-                Sound.twoBeeps();
+            	 detectionCount++; // Increment detection count
+                 LCD.drawString("Count: " + detectionCount, 0, 2);
+                 
+                 if (detectionCount >= 4) {
+                     DEObj.setCMD(3); 
+                     stopRobot();
+                     break;
+                 }
+              Sound.twoBeeps();
+             Sound.twoBeeps();
+
+//Logic 1
                 
                 //Turn left side
-            	leftMotor.setSpeed(10);
-				rightMotor.setSpeed(200);
-				leftMotor.backward();
-				rightMotor.forward();
-				Delay.msDelay(1000);
-				
-							
-				//DEObj.setCMD(1);
+               	leftMotor.setSpeed(150);
+   				rightMotor.setSpeed(300);
+   				leftMotor.backward();
+   				rightMotor.forward();
+   				Delay.msDelay(700);
+   				
+   				//Go forward
+   				leftMotor.setSpeed(200);
+   				rightMotor.setSpeed(200);
+   				leftMotor.forward();
+   				rightMotor.forward();
+   				Delay.msDelay(500);
+   				
+   				//Go Right side
+   				leftMotor.setSpeed(300);
+   				rightMotor.setSpeed(0);
+   				leftMotor.forward();
+   				rightMotor.forward();
+   				Delay.msDelay(900);
+   						
+   				//Go Forward
+   				leftMotor.setSpeed(300);
+   				rightMotor.setSpeed(300);
+   				leftMotor.forward();
+   				rightMotor.forward();
+   				Delay.msDelay(1500);
 
+   				//Go slight right side to go back on line
+   				leftMotor.setSpeed(250);
+   				rightMotor.setSpeed(200);
+   				leftMotor.forward();
+   				rightMotor.forward();
+   				//Delay.msDelay(5000);
+			    DEObj.setCMD(1);
+   //
+/*
+//Logic 2
+                //Turn left side
+             	leftMotor.setSpeed(0);
+   				rightMotor.setSpeed(300);
+   				leftMotor.forward();
+   				rightMotor.forward();
+   				Delay.msDelay(500);
+   				
+   				//Go forward
+   				leftMotor.setSpeed(300);
+   				rightMotor.setSpeed(300);
+   				leftMotor.forward();
+   				rightMotor.forward();
+   				Delay.msDelay(1000);
+   				
+   		
+   				//Go slight right side to go back on line
+   				leftMotor.setSpeed(300);
+   				rightMotor.setSpeed(250);
+   				leftMotor.forward();
+   				rightMotor.forward();
+			    DEObj.setCMD(1);
+*/
+/*Logic 3
+                
+                //Turn left side
+               	leftMotor.setSpeed(150);
+   				rightMotor.setSpeed(300);
+   				leftMotor.backward();
+   				rightMotor.forward();
+   				Delay.msDelay(700);
+   				
+   				//Go forward
+   				leftMotor.setSpeed(200);
+   				rightMotor.setSpeed(200);
+   				leftMotor.forward();
+   				rightMotor.forward();
+   				Delay.msDelay(500);
+   				
+   				//Go Right side
+   				leftMotor.setSpeed(300);
+   				rightMotor.setSpeed(0);
+   				leftMotor.forward();
+   				rightMotor.forward();
+   				Delay.msDelay(900);
+   						
+   				//Go Forward
+   				leftMotor.setSpeed(300);
+   				rightMotor.setSpeed(300);
+   				leftMotor.forward();
+   				rightMotor.forward();
+   				Delay.msDelay(1000);
+
+   				//Go slight right side to go back on line
+   				leftMotor.setSpeed(250);
+   				rightMotor.setSpeed(200);
+   				leftMotor.forward();
+   				rightMotor.forward();
+   				Delay.msDelay(2000);
+   				
+   				leftMotor.setSpeed(200);
+   				rightMotor.setSpeed(250);
+   				leftMotor.forward();
+   				rightMotor.forward();
+   				Delay.msDelay(700);
+   				
+   				leftMotor.setSpeed(200);
+   				rightMotor.setSpeed(200);
+   				leftMotor.forward();
+   				rightMotor.forward();
+   				
+			    DEObj.setCMD(1);
+   */
             }
             else  if(DEObj.getCMD()==1)
             {
@@ -90,7 +207,30 @@ public class LineFollower extends Thread {
                        rightMotor.setSpeed(rightSpeed);      
                        leftMotor.forward();
                        rightMotor.forward();	
-            }          
+            }
+         /*   else if(DEObj.getCMD()==3)
+            {
+            	stopRobot();
+            }
+            */
+            	
         }
+    }
+    
+    public void stopRobot() {
+        // Stop both motors
+        leftMotor.stop();
+        rightMotor.stop();
+        
+        // Stop the stopwatch and get the elapsed time
+        stopwatch.elapsed();
+        long elapsedTime = stopwatch.elapsed();
+        LCD.drawString("Total Time: " + + elapsedTime / 1000 + " sec", 0, 3);
+        LCD.drawString("Finish...",0,4);        
+        musicPlayer.stopMusic();
+
+    }
+    public void stopMusic() {
+        musicPlayer.stopMusic();
     }
 }
